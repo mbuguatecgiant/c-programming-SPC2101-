d26e25c2e2d6c26e0f1c8b5821691398c7cd1570
mbuguatecgiant <mmbugua735@gmail.com>
2025-09-19 20:53:43 +0300

    Add files via upload

---
+++ volume and surface area of a cylinder.C
@@ -0,0 +1,58 @@
+/*
+Name:Mark Mbugua
+Reg No:CT101/G/26487/25
+Description:C program to calculate volume and surface area of a cylinder 
+
+*/
+
+#include<stdio.h>
+
+ int main () { 
+ 
+ 
+ float pi,Radius,height,volume,area;
+ 
+ printf("Enter pi:");
+ scanf("%f",&pi);
+ 
+ printf("\n Enter Radius:");
+ scanf("%f",&Radius);
+ 
+ printf("\n Enter height:");
+ scanf("%f",&height);
+ 
+ volume =pi*Radius*Radius *height ;
+ 
+ printf("\nvolume is cm³ %.3f",volume);
+ 
+   area = 2*pi*Radius*Radius+2*pi*Radius*height;
+ 
+ printf("\n surface area is cm²%.3f",area);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ return 0;
+ 
+ }
